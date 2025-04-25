bd0bb8975ac989e4996b95768705394f61a324b2
Samboja651 <waribekihiko651@gmail.com>
2025-04-25 17:42:11 +0300

    postgres db schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
--- dbms -> mysql
-CREATE DATABASE IF NOT EXISTS crypto_plug;
-USE crypto_plug;
-CREATE TABLE IF NOT EXISTS user(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+-- dbms -> postgresql
+createdb crypto_plug;
+psql crypto_plug;
+CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
     fname VARCHAR(100),
     lname VARCHAR(100),
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -10,21 +10,21 @@ CREATE TABLE IF NOT EXISTS user(
     country VARCHAR(100)
 );
 CREATE TABLE IF NOT EXISTS class(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     start_time TIMESTAMP,
     end_time TIMESTAMP,
     user_id INT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES user(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 CREATE TABLE IF NOT EXISTS brokers(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     refferal_link TEXT,
     user_id INT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES user(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
-CREATE USER IF NOT EXISTS 'admin_trader'@'localhost' IDENTIFIED BY '(PlugTrader2025)';
-GRANT ALL PRIVILEGES ON crypto_plug.* TO 'admin_trader'@'localhost';
-FLUSH PRIVILEGES;
+-- CREATE USER IF NOT EXISTS 'admin_trader'@'localhost' IDENTIFIED BY '(PlugTrader2025)';
+-- GRANT ALL PRIVILEGES ON crypto_plug.* TO 'admin_trader'@'localhost';
+-- FLUSH PRIVILEGES;
 -- end
