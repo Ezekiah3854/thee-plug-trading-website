f3472c1ab4a15e2cf758211a7dc1401a32882a4f
Samboja651 <waribekihiko651@gmail.com>
2025-04-29 09:21:04 +0300

    update unittest

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import os
 import unittest
 import requests
 from dotenv import load_dotenv
-from functions import connect_db
+from functions import connect_db, validate_user_data
 
 load_dotenv('.env')
 
@@ -24,6 +24,16 @@ class TestRoutes(unittest.TestCase):
         """user registration"""
         res = requests.get("http://127.0.0.1:5000/register", timeout=10)
         self.assertEqual(200, res.status_code)
+    
+    def test_user_login(self):
+        """user login"""
+        res = requests.get("http://127.0.0.1:5000/login", timeout=10)
+        self.assertEqual(200, res.status_code)
+    
+    def test_schedule_class(self):
+        """access schedule class"""
+        res = requests.get("http://127.0.0.1:5000/schedule-class", timeout=10)
+        self.assertEqual(200, res.status_code)
 
 class TestFunctions(unittest.TestCase):
     """test helper functions"""
@@ -38,6 +48,32 @@ class TestFunctions(unittest.TestCase):
         """incorrect credential"""
         conn = connect_db(database="incorrect")
         self.assertEqual(type(conn), str)
+
+    # test validation with correct credentials
+    def test_validate_user_data(self):
+        """correct credentials"""
+        result = validate_user_data(
+            "t@gmail.com",
+            "t@Gmail.1",
+            "t@Gmail.1",
+            "Erick",
+            "Onguso",
+            "KE"
+        )
+        self.assertEqual(result, None)
+    
+    # test wrong email
+    def test_validate_user_email(self):
+        """wrong email"""
+        result = validate_user_data(
+            "tgmailcom",
+            "t@Gmail.1",
+            "t@Gmail.1",
+            "Erick",
+            "Onguso",
+            "KE"
+        )
+        self.assertEqual(result, "Email must contain an '@' and '.'")
 
 # -------running the test----------
 # python -m unittest -v
